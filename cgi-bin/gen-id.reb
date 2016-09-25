85df6be5b48f640e72da86170fc975d16fcd76f7
James Irwin <rebolyte@gmail.com>
2016-09-25 23:10:17 +0100

    Fix random ID problem and put JS inside iffy

--- cgi-bin/gen-id.reb
+++ cgi-bin/gen-id.reb
@@ -1,15 +1,69 @@
 REBOL []
 
-generate-id: func [/length len /local out chars] [
-	chars: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
-	out: copy ""
-	if none? len [ len: 8 ]
-	loop len [
-		random/seed form now/precise
-		ind: random/secure length? chars
-		append out pick chars ind
+; Bugged. Doesn't work reliably.
+; generate-id: func [/length len /local out chars] [
+; 	chars: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+; 	out: copy ""
+; 	if none? len [ len: 8 ]
+; 	loop len [
+; 		random/seed form now/precise
+; 		ind: random/secure length? chars
+; 		append out pick chars ind
+; 	]
+; 	out
+; ]
+
+generate-id: func [
+	/length len [integer!]
+	/local bytes rand alphanum key
+][
+	if (fourth system/version) = 3 [
+		make error! "this function uses /dev/urandom, which is not available on windows."
 	]
-	out
+	if none? len [ len: 8 ]
+	bytes: len * 10
+	rand: copy ""
+	alphanum: charset [#"0" - #"9" #"A" - #"Z" #"a" - #"z" #"-" #"_"]
+	key: copy ""
+
+	call/wait/output rejoin ["head -c " bytes " /dev/urandom"] rand
+
+	parse/all rand [
+		some [
+			copy char some alphanum (append key char) |
+			skip
+		]
+	]
+	clear skip key len
+	either (length? key) >= len [
+		return key
+	][
+		; retry
+		generate-id len
+	]
 ]
 
-; loop 5 [ print generate-id ]
+; loop 10 [print generate-id/length 10]
+
+
+; Dad's versions
+; randomize: func [
+; 	"Reseed the random number generator."
+; 	/with seed "date, time, and integer values are used directly; others are converted"
+; ][
+; 	random/seed either find [date! time! integer!] type?/word seed [seed] [
+; 		form any [seed now/precise]
+; 	]
+; ]
+
+; generate-id: does [
+; 	chars: "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+; 	randomize
+; 	rejoin collect [
+; 		repeat i 5 [keep random/only chars]
+; 	]
+; ]
+
+; head collect/into [
+; 	loop 5 [keep random/only chars]
+; ] copy ""
--- src/main.js
+++ src/main.js
@@ -1,172 +1,179 @@
-function ready(fn) {
-	if (document.readyState !== 'loading'){
-		fn();
-	} else {
-		document.addEventListener('DOMContentLoaded', fn);
+/* global Promise, Clipboard */
+
+(function () {
+	'use strict';
+
+	function ready(fn) {
+		if (document.readyState !== 'loading'){
+			fn();
+		} else {
+			document.addEventListener('DOMContentLoaded', fn);
+		}
 	}
-}
 
-function request(type, url, data) {
-	return new Promise(function (resolve, reject) {
-		var xhr = new XMLHttpRequest();
+	function request(type, url, data) {
+		return new Promise(function (resolve, reject) {
+			var xhr = new XMLHttpRequest();
 
-		// true = async
-		xhr.open(type, url, true);
+			// true = async
+			xhr.open(type, url, true);
 
-		xhr.onreadystatechange = function () {
-			var req;
-			if (xhr.readyState === 4) {
-				req = xhr.responseText;
-				if (xhr.status >= 200 && xhr.status < 300) {
-					resolve(req);
-				} else {
-					reject(req);
+			xhr.onreadystatechange = function () {
+				var req;
+				if (xhr.readyState === 4) {
+					req = xhr.responseText;
+					if (xhr.status >= 200 && xhr.status < 300) {
+						resolve(req);
+					} else {
+						reject(req);
+					}
 				}
+			};
+
+			xhr.send(data);
+		});
+	}
+
+	var get = request.bind(this, 'GET');
+	var post = request.bind(this, 'POST');
+
+	var $ = document.querySelector.bind(document);
+	var $$ = document.querySelectorAll.bind(document);
+	Node.prototype.on = Node.prototype.addEventListener;
+
+	function getQueryVar(variable) {
+		var query = window.location.href.split('?')[1];
+		if (!query) {
+			return false;
+		}
+		var vars = query.split('&');
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split('=');
+			if (pair[0] === variable) {
+				return pair[1];
 			}
-		};
-
-		xhr.send(data);
-	});
-}
-
-var get = request.bind(this, 'GET');
-var post = request.bind(this, 'POST');
-
-var $ = document.querySelector.bind(document);
-var $$ = document.querySelectorAll.bind(document);
-Node.prototype.on = Node.prototype.addEventListener;
-
-function getQueryVar(variable) {
-	var query = window.location.href.split('?')[1];
-	if (!query) {
+		}
 		return false;
 	}
-	var vars = query.split('&');
-	for (var i = 0; i < vars.length; i++) {
-		var pair = vars[i].split('=');
-		if (pair[0] === variable) {
-			return pair[1];
-		}
+
+
+	// ---------------------------------------------------------------------------
+
+	var editor = ace.edit('editor');
+
+	function executeCode(code) {
+		post('/cgi-bin/eval.cgi', code).then(function (resp) {
+			$('#output').innerHTML = resp;
+		}).catch(function (err) {
+			$('#output').innerHTML = 'There was a problem evaluating your code.';
+			console.error(err);
+		});
 	}
-	return false;
-}
 
+	function shareCode(code) {
+		post('/cgi-bin/share.cgi', code).then(function (resp) {
+			$('#permalinkFld').value = 'http://localhost/try-red/?s=' + resp;
+			showDialog($('#shareDialog'));
+		}).catch(function (err) {
+			$('#output').innerHTML = 'There was a problem sharing your code.';
+			console.error(err);
+		});
+	}
 
-// ---------------------------------------------------------------------------
+	function loadCode(id) {
+		get('/cgi-bin/get.cgi?s=' + id).then(function (resp) {
+			editor.setValue(resp);
+		}).catch(function (err) {
+			$('#output').innerHTML = 'There was a problem loading this code.';
+			console.error(err);
+		});
+	}
 
-var editor = ace.edit('editor');
+	function showDialog(node) {
+		node.style.display = 'block';
+		$('#underlay').style.display = 'block';
+	}
+	function hideDialog(node) {
+		node.style.display = 'none';
+		$('#underlay').style.display = 'none';
+	}
 
-function executeCode(code) {
-	post('/cgi-bin/eval.cgi', code).then(function (resp) {
-		$('#output').innerHTML = resp;
-	}).catch(function (err) {
-		$('#output').innerHTML = 'There was a problem evaluating your code.';
-		console.error(err);
-	});
-}
+	function init() {
+		editor.setTheme('ace/theme/twilight');
+		editor.session.setMode('ace/mode/plain_text');
 
-function shareCode(code) {
-	post('/cgi-bin/share.cgi', code).then(function (resp) {
-		$('#permalinkFld').value = 'http://localhost/try-red/?s=' + resp;
-		showDialog($('#shareDialog'));
-	}).catch(function (err) {
-		$('#output').innerHTML = 'There was a problem sharing your code.';
-		console.error(err);
-	});
-}
-
-function loadCode(id) {
-	get('/cgi-bin/get.cgi?s=' + id).then(function (resp) {
-		editor.setValue(resp);
-	}).catch(function (err) {
-		$('#output').innerHTML = 'There was a problem loading this code.';
-		console.error(err);
-	});
-}
-
-function showDialog(node) {
-	node.style.display = 'block';
-	$('#underlay').style.display = 'block';
-}
-function hideDialog(node) {
-	node.style.display = 'none';
-	$('#underlay').style.display = 'none';
-}
-
-function init() {
-	editor.setTheme('ace/theme/twilight');
-	editor.session.setMode('ace/mode/plain_text');
-
-	$('#runBtn').on('click', function (evt) {
-		executeCode(editor.getValue());
-	});
-	document.documentElement.on('keydown', function(evt) {
-		if (evt.ctrlKey && evt.which === 13) {
+		$('#runBtn').on('click', function (evt) {
 			executeCode(editor.getValue());
+		});
+		document.documentElement.on('keydown', function (evt) {
+			if (evt.ctrlKey && evt.which === 13) {
+				executeCode(editor.getValue());
+			}
+		});
+
+		$('#shareBtn').on('click', function (evt) {
+			shareCode(editor.getValue());
+		});
+
+		$('#helpBtn').on('click', function (evt) {
+			showDialog($('#helpDialog'));
+		});
+
+		$('#aboutBtn').on('click', function (evt) {
+			showDialog($('#aboutDialog'));
+		});
+
+		Array.prototype.slice.call($$('.dialogCloseBtn'), 0).forEach(function (node) {
+			node.on('click', function (evt) {
+				hideDialog(node.parentNode);
+			});
+		});
+
+		var clipboard = new Clipboard('#copyBtn', {
+			target: function () {
+				return $('#permalinkFld');
+			}
+		});
+
+		clipboard.on('success', function(e) {
+			$('#copyStatus').innerHTML = 'Copied!';
+			setTimeout(function () {
+				$('#copyStatus').innerHTML = '';
+			}, 2000);
+			e.clearSelection();
+		});
+
+		clipboard.on('error', function(e) {
+			$('#copyStatus').innerHTML = 'Not copied. Press ctrl+c to copy.';
+			setTimeout(function () {
+				$('#copyStatus').innerHTML = '';
+			}, 2000);
+		});
+
+		var sId = getQueryVar('s');
+		if (sId) {
+			loadCode(sId);
 		}
-	});
-
-	$('#shareBtn').on('click', function (evt) {
-		shareCode(editor.getValue());
-	});
-
-	$('#helpBtn').on('click', function (evt) {
-		showDialog($('#helpDialog'));
-	});
-
-	$('#aboutBtn').on('click', function (evt) {
-		showDialog($('#aboutDialog'));
-	});
-
-	Array.prototype.slice.call($$('.dialogCloseBtn'), 0).forEach(function (node) {
-		node.on('click', function (evt) {
-			hideDialog(node.parentNode);
-		})
-	});
-
-	var clipboard = new Clipboard('#copyBtn', {
-		target: function () {
-			return $('#permalinkFld');
-		}
-	});
-
-	clipboard.on('success', function(e) {
-		$('#copyStatus').innerHTML = 'Copied!';
-		setTimeout(function () {
-			$('#copyStatus').innerHTML = '';
-		}, 2000);
-		e.clearSelection();
-	});
-
-	clipboard.on('error', function(e) {
-		$('#copyStatus').innerHTML = 'Not copied. Press Cmd+C to copy.';
-		setTimeout(function () {
-			$('#copyStatus').innerHTML = '';
-		}, 2000);
-	});
-
-	var sId = getQueryVar('s');
-	if (sId) {
-		loadCode(sId);
 	}
-}
 
 
-// http://stackoverflow.com/a/27047981/2486583
-var d = document.documentElement.style
-if (('flexWrap' in d) || ('WebkitFlexWrap' in d) || ('msFlexWrap' in d)){
-	// alert('ok');
-} else {
-	alert("You don't appear to be using a browser that supports flexbox. Things may still function, but you will most likely see layout issues.");
-}
+	// http://stackoverflow.com/a/27047981/2486583
+	var d = document.documentElement.style;
+	if (('flexWrap' in d) || ('WebkitFlexWrap' in d) || ('msFlexWrap' in d)){
+		// alert('ok');
+	} else {
+		alert('You don\'t appear to be using a browser that supports flexbox. Things may still function, but you will most likely see layout issues.');
+	}
 
-// Checks for IE 8
-var test = {
-	addEventListener : !!window.addEventListener,
-	querySelectorAll : !!document.querySelectorAll,
-};
-if (test.addEventListener && test.querySelectorAll) {
-	ready(init);
-} else {
-	alert("You appear to be using a very old browser. To use this app, please upgrade to at least Internet Explorer 9 or a more modern browser.");
-}
+	// Checks for IE 8
+	var test = {
+		addEventListener : !!window.addEventListener,
+		querySelectorAll : !!document.querySelectorAll,
+	};
+	if (test.addEventListener && test.querySelectorAll) {
+		ready(init);
+	} else {
+		alert('You appear to be using a very old browser. To use this app, please upgrade to at least Internet Explorer 9 or a more modern browser.');
+	}
+
+}());
